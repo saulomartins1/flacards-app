f82e504b7b97bb81a1ad1feadb71e4f97cdd99ef
d3Niro <saulomartins2020@gmail.com>
2024-02-03 18:44:57 -0300

    feat: UI home page

---
+++ app/(pages)/me/(pages)/decks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>Meus baralhos</div>
+    )
+}
+
+export default page
--- app/(pages)/me/page.tsx
+++ app/(pages)/me/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/app/components/ui/button';
 import { getUserSession } from '@/app/lib/supabase/actions'
-import { PlusCircleIcon } from 'lucide-react';
+import { ChevronRight, PlayCircleIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React from 'react'
-import { Deck } from './components/Deck';
 
 async function page() {
 
@@ -13,19 +14,18 @@ async function page() {
 
 
     return <section className='flex items-center justify-center mt-16'>
-        <div className='flex flex-col items-center gap-6'>
-            <h1 className='text-3xl font-bold'>Meus Baralhos</h1>
+        <div className='flex flex-col items-center gap-16'>
             <div className='flex flex-col items-center gap-8'>
-                <div className='flex gap-5 items-center'>
-                    <p><span className='font-semibold text-2xl mr-3'>02</span> baralhos</p>
-                    <p><span className='font-semibold text-2xl mr-3'>66</span> cards</p>
-                </div>
-                <Button className='flex font-semibold items-center gap-2'><PlusCircleIcon /> Criar Baralho</Button>
+                <Image src="/ui-hero.png" width="180" height="155" alt='Flacards logo from app home page' priority />
+                <Link href="/me/decks"><Button className='flex font-semibold items-center gap-2 bg-brand'><PlayCircleIcon /> Jogar Agora</Button></Link>
             </div>
-
-            {/* <div className='flex flex-col justify-center items-center gap-4'>
-                <Deck />
-            </div> */}
+            <div className='flex flex-col items-center gap-5'>
+                <div className='flex gap-6 items-center'>
+                    <p><span className='font-semibold text-2xl mr-1'>02</span> baralhos</p>
+                    <p><span className='font-semibold text-2xl mr-1'>66</span> cards</p>
+                </div>
+                <Link href="/me/decks" className='flex gap-3 items-center text-sm font-medium p-3'>Ver baralhos <ChevronRight size={16} /></Link>
+            </div>
         </div>
     </section>
 }
