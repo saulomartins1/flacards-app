09e3bc6df1c0a5dded8fe4267a2b94f33938eedd
d3Niro <saulomartins2020@gmail.com>
2024-02-05 19:04:03 -0300

    style: Deck component

--- app/(pages)/me/(pages)/decks/page.tsx
+++ app/(pages)/me/(pages)/decks/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Button } from "@/app/components/ui/button";
+import { getUserSession } from "@/app/lib/supabase/actions";
+import { PlusCircleIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+import { Deck } from "../../components/Deck";
 
-function page() {
-    return (
-        <div>Meus baralhos</div>
-    )
+async function page() {
+  const {
+    data: { user },
+  } = await getUserSession();
+
+  if (!user) redirect("/acessar");
+
+  return (
+    <section className="flex items-center justify-center mt-16">
+      <div className="flex flex-col items-center gap-16">
+        <div className="flex flex-col items-center gap-8">
+          <h1 className="text-3xl font-bold">Meus Baralhos</h1>
+          <div className="flex flex-col items-center gap-5">
+            <div className="flex gap-6 items-center">
+              <p>
+                <span className="font-semibold text-2xl mr-1">02</span> baralhos
+              </p>
+              <p>
+                <span className="font-semibold text-2xl mr-1">66</span> cards
+              </p>
+            </div>
+          </div>
+          <Button className="flex font-semibold items-center gap-2">
+            <PlusCircleIcon /> Criar Baralho
+          </Button>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <Deck />
+          <Deck />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default page
+export default page;
--- app/(pages)/me/components/Deck.tsx
+++ app/(pages)/me/components/Deck.tsx
@@ -1,22 +1,47 @@
-"use client"
-import { GalleryHorizontalEnd, Trash2, Edit } from 'lucide-react'
-import React from 'react'
+"use client";
+import { Button } from "@/app/components/ui/button";
+import {
+  GalleryHorizontalEnd,
+  //   Trash2,
+  Edit,
+  PlayCircleIcon,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export function Deck() {
+  const handleEditDeck = () => {
+    alert("Edit Deck");
+  };
 
-    const handleEditDeck = () => {
-        alert("Edit Deck");
-    }
-    const handleDeleteDeck = () => {
-        alert("Delete Deck");
-    }
+  //   const handleDeleteDeck = () => {
+  //     alert("Delete Deck");
+  //   };
 
-    return <article className='w-full flex-wrap flex items-center bg-card gap-16 p-5 rounded-md border border-border'>
-        <h2 className='truncate max-w-52'>JavaScript Conceitos</h2>
-        <div className='flex items-center gap-8'>
-            <span className='flex items-center gap-2' title="Cards neste baralho"><GalleryHorizontalEnd />18</span>
-            <button onClick={handleEditDeck}><Edit /> </button >
-            <button onClick={handleDeleteDeck}><Trash2 /> </button >
-        </div>
+  return (
+    <article className="w-full flex-wrap flex items-center bg-card gap-16 p-5 rounded-md border border-border">
+      <div className="flex items-center w-full justify-between">
+        <h2 className="truncate max-w-64">
+          JavaScript Conceitos Lorem ipsum dolor sit amet.
+        </h2>
+        <button onClick={handleEditDeck}>
+          <Edit />
+        </button>
+      </div>
+      <div className="w-full flex justify-between items-center gap-8">
+        <span className="flex items-center gap-2" title="Cards neste baralho">
+          <GalleryHorizontalEnd />2
+        </span>
+        <Link href="/me/decks">
+          <Button className="flex font-semibold items-center gap-2 bg-brand">
+            <PlayCircleIcon /> Jogar
+          </Button>
+        </Link>
+
+        {/* <button onClick={handleDeleteDeck}>
+          <Trash2 />{" "}
+        </button> */}
+      </div>
     </article>
+  );
 }
